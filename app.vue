9997ffd11cabad0c5fc429ef0627fa2aa2702f20
erenaygunn <eaygun178@gmail.com>
2024-06-14 19:41:43 +0300

    icon states

--- app.vue
+++ app.vue
@@ -1,4 +1,41 @@
 <template>
-  <div>
+  <h2>Theme 1</h2>
+  <div class="main">
+    <div class="container">
+      <MainButton theme="theme1" class="primary" label="Primary Button"/>
+      <MainButton theme="theme1" class="primary" label="Primary Button" icon=""/>
+      <MainButton theme="theme1" class="primary" label="Primary Button" :disabled="true"/>
+      <MainButton theme="theme1" class="primary" label="Primary Button" :loading="true"/>
+    </div>
+    <div class="container">
+      <MainButton theme="theme1" class="secondary" label="Secondary Button"/>
+      <MainButton theme="theme1" class="secondary" label="Secondary Button" :disabled="true"/>
+      <MainButton theme="theme1" class="secondary" label="Secondary Button" :loading="true"/>
+    </div>
+    <div class="container">
+      <MainButton theme="theme1" class="outlined" label="Outlined Button"/>
+      <MainButton theme="theme1" class="outlined" label="Outlined Button" :disabled="true"/>
+      <MainButton theme="theme1" class="outlined" label="Outlined Button" :loading="true"/>
+    </div>
   </div>
 </template>
+
+<script>
+</script>
+
+<style>
+.main {
+  display: flex;
+  align-items: start;
+  flex-direction: row;
+  margin-top: 20px;
+  gap: 30px;
+}
+.container {
+  display: flex;
+  align-items: start;
+  flex-direction: column;
+  gap: 20px;
+}
+
+</style>
--- components/MainButton.vue
+++ components/MainButton.vue
@@ -1,7 +1,8 @@
 <template>
-    <button :class="[theme, {'loading': loading,'disabled': disabled}]" @click="handleClick">
+    <button :class="[theme, icon, {'loading': loading,'disabled': disabled}]" @click="handleClick">
+        <img v-if="icon && !disabled" src="@/assets/right.png" alt="icon" :class=icon>
         <span v-if="loading">Loading...</span>
-        <span v-else>{{ label }}</span>
+        <span v-else class="label">{{ label }}</span>
     </button>
 </template>
 
@@ -20,6 +21,10 @@ export default {
             type: Boolean,
             default: false
         },
+        icon: {
+            type: String,
+            default: ''
+        },
         theme: {
             type: String,
             default: ''
@@ -36,4 +41,60 @@ export default {
 </script>
 
 <style scoped>
+.primary, .secondary, .outlined {
+    border: none;
+    padding: 14px 17px;
+    cursor: pointer;
+    background: none;
+    display: flex;
+    align-items: center;
+}
+
+.outlined {
+    border: 2px solid;
+}
+
+button img{
+    width: 20px;
+    display: inline-block;
+}
+
+button.left {
+    flex-direction: row;
+}
+button.right {
+    flex-direction: row-reverse;
+}
+button.only-icon .label{
+    display: none;
+}
+
+/* Theme 1 */
+
+/* For primary button  */
+.theme1.primary {
+  background-color: #000;
+  color: #fff;
+  border-radius: 10px;
+  transition: all 0.2s;
+}
+.theme1.primary:hover {
+    background-color: #505050;
+    color: #fff;
+}
+.theme1.primary img {
+    filter: invert(1);
+}
+
+/* For secondary button  */
+.theme1.secondary {
+  background-color: #00924e;
+  color: #000;
+  border-radius: 10px;
+  transition: all 0.2s;
+}
+.theme1.secondary:hover {
+    background-color: #00b25c;
+    color: #000;
+}
 </style>
