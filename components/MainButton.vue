2edf82645ebf57b20107bd14066aa5438551cc9d
erenaygunn <eaygun178@gmail.com>
2024-06-14 14:09:48 +0300

    Button structure

---
+++ components/MainButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <button :class="[theme, {'loading': loading,'disabled': disabled}]" @click="handleClick">
+        <span v-if="loading">Loading...</span>
+        <span v-else>{{ label }}</span>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        loading: {
+            type: Boolean,
+            default: false
+        },
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        theme: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+        handleClick() {
+            if (!this.loading && !this.disabled) {
+                // Handle button click logic here
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
