9ed24dc03c2dd41fd6b87aaf63a4e4951b94fb4f
erenaygunn <eaygun178@gmail.com>
2024-06-25 16:00:19 +0300

    Dynamically fetch buttons

--- components/Buttons.vue
+++ components/Buttons.vue
@@ -1,58 +1,70 @@
 <template>
-    <div class="app">
-      <div class="column">
-        <div class="main">
-          <div class="container">
-            <PrimaryButton>Primary Button</PrimaryButton>
-            <PrimaryButton icon="only-icon">Primary Button</PrimaryButton>
-            <PrimaryButton icon="left">Primary Button</PrimaryButton>
-            <PrimaryButton icon="right">Primary Button</PrimaryButton>
-          </div>
-          <div class="container">
-            <SecondaryButton>Secondary Button</SecondaryButton>
-            <SecondaryButton icon="only-icon">Secondary Button</SecondaryButton>
-            <SecondaryButton icon="left">Secondary Button</SecondaryButton>
-            <SecondaryButton icon="right">Secondary Button</SecondaryButton>
-          </div>
-          <div class="container">
-            <OutlinedButton>Outlined Button</OutlinedButton>
-            <OutlinedButton icon="only-icon">Outlined Button</OutlinedButton>
-            <OutlinedButton icon="left">Outlined Button</OutlinedButton>
-            <OutlinedButton icon="right">Outlined Button</OutlinedButton>
-          </div>
-        </div>
-        <div class="container">
-          <StateUpdater />
-          <h2>Or Click on Buttons to update states</h2>
+  <div class="app">
+    <div class="column">
+      <div class="main">
+        <div class="container" v-for="(buttonGroup, index) in groupedButtons" :key="index">
+          <Component
+            v-for="(button, buttonIndex) in buttonGroup"
+            :is="button.type === 'PrimaryButton' ? PrimaryButton
+            :button.type === 'SecondaryButton' ? SecondaryButton
+            :OutlinedButton"
+            :key="buttonIndex"
+            :icon="button.icon"
+          >
+            {{ button.label }}
+          </Component>
         </div>
       </div>
+      <div class="container">
+        <StateUpdater />
+        <h2>Or Click on Buttons to update states</h2>
+      </div>
     </div>
-  </template>
-  
-  <style scoped>
-  .app {
-    display: flex;
-    align-items: start;
-    flex-direction: row;
+  </div>
+</template>
+
+<script setup>
+import { PrimaryButton, SecondaryButton, OutlinedButton } from '#components';
+
+const data = await $fetch('http://localhost:8000/buttons');
+
+const groups = {};
+
+data.forEach((button) => {
+  if (!groups[button.type]) {
+    groups[button.type] = [];
   }
-  
-  .main {
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    gap: 30px;
-  }
-  
-  .column {
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-  }
-  
-  .container {
-    display: flex;
-    align-items: start;
-    flex-direction: column;
-    gap: 20px;
-  }
-  </style>
+  groups[button.type].push(button);
+});
+
+const groupedButtons = Object.values(groups);
+
+</script>
+
+<style scoped>
+.app {
+  display: flex;
+  align-items: start;
+  flex-direction: row;
+}
+
+.main {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 30px;
+}
+
+.column {
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+}
+
+.container {
+  display: flex;
+  align-items: start;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
--- components/PrimaryButton.vue
+++ components/PrimaryButton.vue
@@ -104,4 +104,3 @@ button.loading {
   
 }
 </style>
-~/store/buttonStore
